566d37a7de64a70c38835d3a3a7059d2c610ee9c
fahadjunaid60 <fahadjunaid60@gmail.com>
2020-10-04 19:08:41 +0800

    server.php

--- public/js/app.js
+++ public/js/app.js
@@ -49824,8 +49824,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Fad Junaid\Desktop\personal-project\src\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Fad Junaid\Desktop\personal-project\src\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Web Projects\znmc\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Web Projects\znmc\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
